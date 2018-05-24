e647aa7eb5f01e3cf8520cefaffd14cad4daef96
Marcus Bannerman <m.bannerman@gmail.com>
2018-05-24 10:29:53 +0100

    Now have JSON communication working

--- Arduino/FastTracker/FastTracker.ino
+++ Arduino/FastTracker/FastTracker.ino
@@ -3,7 +3,7 @@
 
 
 #include "DMAChannel.h"
-#include "ADC.h" 
+#include "ADC.h"
 #include <ArduinoJson.h>
 
 #define ADC_conv_speed ADC_CONVERSION_SPEED::MED_SPEED
@@ -26,117 +26,134 @@ const uint16_t ChannelsCfg_1 [] =  { 0x45, 0x46, 0x47, 0x44 };  //ADC1: CH0 ad4(
 
 const int ledPin = 13;
 
-DMAMEM static volatile uint16_t __attribute__((aligned(BUF_SIZE+0))) adcbuffer_0[BUF_SIZE];
-DMAMEM static volatile uint16_t __attribute__((aligned(BUF_SIZE+0))) adcbuffer_1[BUF_SIZE];
+DMAMEM static volatile uint16_t __attribute__((aligned(BUF_SIZE + 0))) adcbuffer_0[BUF_SIZE];
+DMAMEM static volatile uint16_t __attribute__((aligned(BUF_SIZE + 0))) adcbuffer_1[BUF_SIZE];
 
 volatile int d2_active;
 
 void setup() {
   // initialize the digital pin as an output.
   pinMode(ledPin, OUTPUT);
-  delay(500); 
+  delay(500);
 
   d2_active = 0;
-  
+
   pinMode(2, INPUT_PULLUP);
   pinMode(4, OUTPUT);
   pinMode(6, OUTPUT);
 
   attachInterrupt(2, d2_isr, FALLING);
-  
+
   Serial.begin(115200);
-  
+
   // clear buffer
-  for (int i = 0; i < BUF_SIZE; ++i){
-      adcbuffer_0[i] = 50000;
-      adcbuffer_1[i] = 50000;
-    
+  for (int i = 0; i < BUF_SIZE; ++i) {
+    adcbuffer_0[i] = 50000;
+    adcbuffer_1[i] = 50000;
+
   }
-   
+
   setup_adc();
-  setup_dma(); 
-  
+  setup_dma();
+
 }
 elapsedMillis debounce;
 
 
 void loop() {
-DynamicJsonBuffer jBuffer;
-JsonObject& root = jBuffer.parseObject(Serial, 1);
-if (!root.success())
-  return;
+  DynamicJsonBuffer jsonBuffer;
+  JsonObject& json_in_root = jsonBuffer.parseObject(Serial, 1);
+  if (!json_in_root.success())
+    return;
 
-if (root.containsKey("CMD") && root.is<const char*>("CMD"))
-  Serial.println("Hey I have a command");
+  const JsonVariant& cmd = json_in_root["CMD"];
+  if (cmd.is<int>()) {
+    switch (cmd.as<int>()) {
+      case 0: {
+          //CMD0 is a info request
+          jsonBuffer.clear(); //Save memory by clearing the jBuffer for reuse, we can't use json_in_root or anything from it after this though!
+          JsonObject& json_out_root = jsonBuffer.createObject();
+          json_out_root["Status"] = "Success";
+          json_out_root["CompileTime"] = __DATE__ " " __TIME__;
+          json_out_root.printTo(Serial);
+          Serial.print('\n');
+          break;
+        }
+      case 1: {
+          Serial.print("{\"Status\":\"Success\", \"Result\":[");
+          for (int i = 0; i < BUF_SIZE; i = i + 4) {
+            Serial.print("[");
+            Serial.print(adcbuffer_0[i]);
+            Serial.print(",");            
+            Serial.print(adcbuffer_0[i+1]);
+            Serial.print(",");            
+            Serial.print(adcbuffer_0[i+2]);
+            Serial.print(",");            
+            Serial.print(adcbuffer_0[i+3]);
+            Serial.print(",");            
+            Serial.print(adcbuffer_1[i]);
+            Serial.print(",");            
+            Serial.print(adcbuffer_1[i+1]);
+            Serial.print(",");            
+            Serial.print(adcbuffer_1[i+2]);
+            Serial.print(",");            
+            Serial.print(adcbuffer_1[i+3]);
+            Serial.print("]");
+          }
+          Serial.println("]}");
 
-root.prettyPrintTo(Serial);
+          digitalWrite(ledPin, HIGH);   // set the LED on
+          delay(50);                  // wait for a second
+          digitalWrite(ledPin, LOW);    // set the LED off
+          break;
+        }
+      default: {
+          jsonBuffer.clear(); //Save memory by clearing the jBuffer for reuse, we can't use json_in_root or anything from it after this though!
+          JsonObject& json_out_root = jsonBuffer.createObject();
+          json_out_root["Status"] = "Fail";
+          json_out_root["Error"] = "Unrecognised command";
+          json_out_root.printTo(Serial);
+          Serial.print('\n');
+          break;
+        }
+    }
+  }
 
 
-  while(0){     //this is leftover from the original example, it no longer serves a purpose
+  while (0) {   //this is leftover from the original example, it no longer serves a purpose
     uint8_t pin = 0;
-    if(d2_active) pin=1;
+    if (d2_active) pin = 1;
 
-    if(pin>0){
-      
-      for (int i = 0; i < BUF_SIZE; i = i + 4){
-        int a = adcbuffer_0[i];
-        Serial.print(a);
-        Serial.print(".");
-        int b = adcbuffer_0[i+1];
-        Serial.print(b);
-        Serial.print(".");
-        int c = adcbuffer_0[i+2];
-        Serial.print(c);
-        Serial.print(".");
-        int d = adcbuffer_0[i+3];
-        Serial.print(d);
-        Serial.print("...");                
-        int e = adcbuffer_1[i];
-        Serial.print(e);        
-        Serial.print(".");
-        int f = adcbuffer_1[i+1];
-        Serial.print(f);        
-        Serial.print(".");
-        int g = adcbuffer_1[i+2];
-        Serial.print(g);        
-        Serial.print(".");                        
-        int h = adcbuffer_1[i+3];
-        Serial.println(h);
+    if (pin > 0) {
 
-      }
-
-      digitalWrite(ledPin, HIGH);   // set the LED on
-      delay(50);                  // wait for a second
-      digitalWrite(ledPin, LOW);    // set the LED off
-
-      d2_active = 0;     
+      d2_active = 0;
     }
   }
 
   digitalWrite(ledPin, HIGH);   // set the LED on
   delay(100);                  // wait for a second
   digitalWrite(ledPin, LOW);    // set the LED off
-  delay(100);   
+  delay(100);
 }
 
 void setup_dma() {
-  dma0->begin(true);              // allocate the DMA channel 
-  dma0->TCD->SADDR = &ADC0_RA;    // where to read from
-  dma0->TCD->SOFF = 0;            // source increment each transfer
-  dma0->TCD->ATTR = 0x101;
-  dma0->TCD->NBYTES = 2;     // bytes per transfer
-  dma0->TCD->SLAST = 0;
+  dma0->begin(true);              // allocate the DMA channel
+  dma0->TCD->SADDR = &ADC0_RA;    // where to read from (The ADC result)
+  dma0->TCD->SOFF = 0;            // source increment each transfer (0=Don't move from the ADC result)
+  dma0->TCD->ATTR = 0x101;        // [00000][001][00000][001] [Source Address Modulo=off][Source data size=1][Destination address modulo=0][Destination size=1] pg 554  Used for circular buffers, not needed here
+  dma0->TCD->NBYTES = 2;          // bytes per transfer
+  dma0->TCD->SLAST = 0;           //SLAST 
   dma0->TCD->DADDR = &adcbuffer_0[0];// where to write to
-  dma0->TCD->DOFF = 2; 
-  dma0->TCD->DLASTSGA = -2*BUF_SIZE;
+  dma0->TCD->DOFF = 2;
+  dma0->TCD->DLASTSGA = -2 * BUF_SIZE;
   dma0->TCD->BITER = BUF_SIZE;
-  dma0->TCD->CITER = BUF_SIZE;    
+  dma0->TCD->CITER = BUF_SIZE;
   dma0->triggerAtHardwareEvent(DMAMUX_SOURCE_ADC0);
   dma0->disableOnCompletion();    // require restart in code
   dma0->interruptAtCompletion();
   dma0->attachInterrupt(dma0_isr);
-  
-  dma1->begin(true);              // allocate the DMA channel 
+
+  dma1->begin(true);              // allocate the DMA channel
   dma1->TCD->SADDR = &ChannelsCfg_0[0];
   dma1->TCD->SOFF = 2;            // source increment each transfer (n bytes)
   dma1->TCD->ATTR = 0x101;
@@ -150,23 +167,23 @@ void setup_dma() {
   dma1->triggerAtTransfersOf(*dma0);
   dma1->triggerAtCompletionOf(*dma0);
 
-  dma2->begin(true);              // allocate the DMA channel 
+  dma2->begin(true);              // allocate the DMA channel
   dma2->TCD->SADDR = &ADC1_RA;    // where to read from
   dma2->TCD->SOFF = 0;            // source increment each transfer
   dma2->TCD->ATTR = 0x101;
   dma2->TCD->NBYTES = 2;     // bytes per transfer
   dma2->TCD->SLAST = 0;
   dma2->TCD->DADDR = &adcbuffer_1[0];// where to write to
-  dma2->TCD->DOFF = 2; 
-  dma2->TCD->DLASTSGA = -2*BUF_SIZE;
+  dma2->TCD->DOFF = 2;
+  dma2->TCD->DLASTSGA = -2 * BUF_SIZE;
   dma2->TCD->BITER = BUF_SIZE;
-  dma2->TCD->CITER = BUF_SIZE;    
+  dma2->TCD->CITER = BUF_SIZE;
   dma2->triggerAtHardwareEvent(DMAMUX_SOURCE_ADC1);
   dma2->disableOnCompletion();    // require restart in code
   dma2->interruptAtCompletion();
   dma2->attachInterrupt(dma2_isr);
-  
-  dma3->begin(true);              // allocate the DMA channel 
+
+  dma3->begin(true);              // allocate the DMA channel
   dma3->TCD->SADDR = &ChannelsCfg_1[0];
   dma3->TCD->SOFF = 2;            // source increment each transfer (n bytes)
   dma3->TCD->ATTR = 0x101;
@@ -182,7 +199,7 @@ void setup_dma() {
 
   dma0->enable();
   dma1->enable();
-  
+
   dma2->enable();
   dma3->enable();
 }
@@ -194,41 +211,43 @@ void setup_adc() {
   adc->setConversionSpeed(ADC_conv_speed, ADC_0); // change the conversion speed
   adc->setSamplingSpeed(ADC_samp_speed, ADC_0); // change the sampling speed
   adc->adc0->setReference(ADC_REFERENCE::REF_3V3);
-  
+
   //ADC1
   //adc->setAveraging(16, ADC_1); // set number of averages
   adc->adc1->setResolution(12); // set bits of resolution
   adc->setConversionSpeed(ADC_conv_speed, ADC_1); // change the conversion speed
   adc->setSamplingSpeed(ADC_samp_speed, ADC_1); // change the sampling speed
   adc->adc1->setReference(ADC_REFERENCE::REF_3V3);
-  
+
   ADC1_CFG2 |= ADC_CFG2_MUXSEL;
-  
+
   adc->adc0->enableDMA(); //ADC0_SC2 |= ADC_SC2_DMAEN;  // using software trigger, ie writing to ADC0_SC1A
   adc->adc1->enableDMA();
-  
-} 
+
+}
 
 void d2_isr(void) {
-  if(debounce > 200){
+  if (debounce > 200) {
     d2_active = 1;
     debounce = 0;
-    }
-    else{return;}
+  }
+  else {
+    return;
+  }
 }
 
 void dma0_isr(void) {
-    dma0->TCD->DADDR = &adcbuffer_0[0];
-    dma0->clearInterrupt();
-    dma0->enable();
-    digitalWriteFast(4, HIGH);
-    digitalWriteFast(4, LOW);
+  dma0->TCD->DADDR = &adcbuffer_0[0];
+  dma0->clearInterrupt();
+  dma0->enable();
+  digitalWriteFast(4, HIGH);
+  digitalWriteFast(4, LOW);
 }
 
 void dma2_isr(void) {
-    dma2->TCD->DADDR = &adcbuffer_1[0];
-    dma2->clearInterrupt();
-    dma2->enable();
-    digitalWriteFast(6, HIGH);
-    digitalWriteFast(6, LOW);
+  dma2->TCD->DADDR = &adcbuffer_1[0];
+  dma2->clearInterrupt();
+  dma2->enable();
+  digitalWriteFast(6, HIGH);
+  digitalWriteFast(6, LOW);
 }
