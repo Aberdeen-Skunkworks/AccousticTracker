0531f69682813d173441ae91b9c58e88b4a92ac7
Marcus Bannerman <m.bannerman@gmail.com>
2018-05-25 08:30:17 +0100

    Increased sampling duration, and cleaned up the code a little with more comments

--- Arduino/FastTracker/FastTracker.ino
+++ Arduino/FastTracker/FastTracker.ino
@@ -5,66 +5,79 @@
 #define ADC_conv_speed ADC_CONVERSION_SPEED::VERY_HIGH_SPEED
 #define ADC_samp_speed ADC_SAMPLING_SPEED::VERY_HIGH_SPEED
 
-#define BUF_SIZE 256
+//The size of the DMA/ADC buffers. Sizes above 512 don't seem to work, which the datasheet agrees with if the DMA channels are in ELINK mode, which they must be.
+#define BUF_SIZE 512
+//DMAMEM is just a hint to allocate this in the lower memory addresses. As the stack is in the upper addresses, this should result in the DMA using a different memory controller to the CPU, so neither have to wait for each other
+DMAMEM static volatile uint16_t __attribute__((aligned(BUF_SIZE + 0))) adcbuffer_0[BUF_SIZE];
+DMAMEM static volatile uint16_t __attribute__((aligned(BUF_SIZE + 0))) adcbuffer_1[BUF_SIZE];
 
+//ChannelsCfg order must be {CH1, CH2, CH3, CH0 }, adcbuffer output will be CH0, CH1, CH2, CH3
+//Order must be {Second . . . . . . . . First} no matter the number of channels used.
+DMAMEM static volatile uint16_t ChannelsCfg_0 [] =  { 0x46, 0x46, 0x46, 0x46 };  //ADC0: CH0 ad6(A6), CH1 ad7(A7), CH2 ad15(A8), CH3 ad4(A9)
+DMAMEM static volatile uint16_t ChannelsCfg_1 [] =  { 0x45, 0x46, 0x47, 0x44 };  //ADC1: CH0 ad4(A17), CH1 ad5(A16), CH2ad6(A18), CH3 ad7(A19)
+
+//Create the ADC and DMA controller objects
 ADC *adc = new ADC(); // adc object
-
 DMAChannel* dma0 = new DMAChannel(false);
 DMAChannel* dma1 = new DMAChannel(false);
 DMAChannel* dma2 = new DMAChannel(false);
 DMAChannel* dma3 = new DMAChannel(false);
 
-
-//ChannelsCfg order must be {CH1, CH2, CH3, CH0 }, adcbuffer output will be CH0, CH1, CH2, CH3
-//Order must be {Second . . . . . . . . First} no matter the number of channels used.
-DMAMEM static volatile uint16_t __attribute__((aligned(BUF_SIZE + 0))) ChannelsCfg_0 [] =  { 0x46, 0x46, 0x46, 0x46 };  //ADC0: CH0 ad6(A6), CH1 ad7(A7), CH2 ad15(A8), CH3 ad4(A9)
-DMAMEM static volatile uint16_t __attribute__((aligned(BUF_SIZE + 0))) ChannelsCfg_1 [] =  { 0x45, 0x46, 0x47, 0x44 };  //ADC1: CH0 ad4(A17), CH1 ad5(A16), CH2ad6(A18), CH3 ad7(A19)
-
+//Shortcut for the LED pin
 const int ledPin = 13;
 
-DMAMEM static volatile uint16_t __attribute__((aligned(BUF_SIZE + 0))) adcbuffer_0[BUF_SIZE];
-DMAMEM static volatile uint16_t __attribute__((aligned(BUF_SIZE + 0))) adcbuffer_1[BUF_SIZE];
-
 void setup() {
-  // initialize the digital pin as an output.
+  //Setup the led
   pinMode(ledPin, OUTPUT);
   delay(500);
-  pinMode(20, OUTPUT);
 
   Serial.begin(115200);
 
-  // clear buffer
+  // clear DMA buffer (helps us see when a ADC trigger did not work). Not actually needed.
   for (int i = 0; i < BUF_SIZE; ++i) {
     adcbuffer_0[i] = 50000;
     adcbuffer_1[i] = 50000;
 
   }
-
+  //Setup the ADC
   setup_adc();
+  //Setup the DMA channels
   setup_dma();
-
 }
 
+//These are for the small PWM/Square-wave generator we've implemented so that we can generate pulses exactly timed with the ADC
+//This counts how many transitions have been outputted so far
 volatile int pwm_counter = 0;
-volatile int pwm_pin = 20;
+//This is the maximum number of transitions to complete in a single pulse
 volatile int pwm_pulse_width = 8;
+//The digital pin to output the pulse on
+volatile int pwm_pin = 20;
+//The timer interrupt used to generate the square wave
 IntervalTimer pwm_timer;
 
-
+//This is the callback, called by pwm_timer.
 void pwm_isr(void) {
+  //First check the current pin state
   bool oldState = digitalRead(pwm_pin);
+  //Toggle the pin state to cause a transition
   digitalWrite(pwm_pin, !oldState);//Toggle
+  //Increase the counter and check if that's the end of the pulse
   pwm_counter += 1;
-  if (pwm_counter > pwm_pulse_width)
+  if (pwm_counter > pwm_pulse_width) {
+    pinMode(pwm_pin, INPUT_DISABLE);
     pwm_timer.end();
+  }
 }
 
+//This callback is called whenever a DMA channel has completed all BUF_SIZE reads from its ADC. It resets the DMA controller back to the start clears the interrupt.
 void dma0_isr(void) {
   dma0->TCD->DADDR = &adcbuffer_0[0];
   dma0->clearInterrupt();
+  //Uncomment below if you want the ADC to continuously sample the pin.
   //dma0->enable();
 }
 
+//Exactly the same as dma0_isr
 void dma2_isr(void) {
   dma2->TCD->DADDR = &adcbuffer_1[0];
   dma2->clearInterrupt();
@@ -72,97 +85,99 @@ void dma2_isr(void) {
 }
 
 void loop() {
+  //Try to parse the JSON commands coming in via the serial port
   DynamicJsonBuffer jsonBuffer;
   JsonObject& json_in_root = jsonBuffer.parseObject(Serial, 2);
   if (!json_in_root.success())
+    //Parsing failed, try again later
     return;
 
   const JsonVariant& cmd = json_in_root["CMD"];
-  if (cmd.is<int>()) {
-    switch (cmd.as<int>()) {
-      case 0: {
-          //CMD0 is a info request
-          jsonBuffer.clear(); //Save memory by clearing the jBuffer for reuse, we can't use json_in_root or anything from it after this though!
-          JsonObject& json_out_root = jsonBuffer.createObject();
-          json_out_root["Status"] = "Success";
-          json_out_root["CompileTime"] = __DATE__ " " __TIME__;
-          json_out_root.printTo(Serial);
-          Serial.print('\n');
-          break;
+  if (!cmd.is<int>()) {
+    Serial.print("{\"Status\":\"Fail\", \"Error\":\"CMD is not a integer?\"}\n");
+    return;
+  }
+  
+  switch (cmd.as<int>()) {
+    case 0: {
+        //CMD0 is a info request
+        jsonBuffer.clear(); //Save memory by clearing the jBuffer for reuse, we can't use json_in_root or anything from it after this though!
+        JsonObject& json_out_root = jsonBuffer.createObject();
+        json_out_root["Status"] = "Success";
+        json_out_root["CompileTime"] = __DATE__ " " __TIME__;
+        json_out_root.printTo(Serial);
+        Serial.print('\n');
+        break;
+      }
+    case 1: {
+        //CMD 1 Reads out the results of the last/ongoing conversion
+
+        Serial.print("{\"Status\":\"Success\", \"ResultADC0\":[");
+        for (int i = 0; i < BUF_SIZE; i = i + 4) {
+          Serial.print(adcbuffer_0[i]);
+          Serial.print(",");
+          Serial.print(adcbuffer_0[i + 1]);
+          Serial.print(",");
+          Serial.print(adcbuffer_0[i + 2]);
+          Serial.print(",");
+          Serial.print(adcbuffer_0[i + 3]);
+          if (i != BUF_SIZE - 4)
+            Serial.print(",");
         }
-      case 1: {
-          //Read out the results of the last conversion!
-
-          Serial.print("{\"Status\":\"Success\", \"ResultADC0\":[");
-          for (int i = 0; i < BUF_SIZE; i = i + 4) {
-            Serial.print(adcbuffer_0[i]);
+        Serial.print("], \"ResultADC1\":[");
+        for (int i = 0; i < BUF_SIZE; i = i + 4) {
+          Serial.print(adcbuffer_1[i]);
+          Serial.print(",");
+          Serial.print(adcbuffer_1[i + 1]);
+          Serial.print(",");
+          Serial.print(adcbuffer_1[i + 2]);
+          Serial.print(",");
+          Serial.print(adcbuffer_1[i + 3]);
+          if (i != BUF_SIZE - 4)
             Serial.print(",");
-            Serial.print(adcbuffer_0[i + 1]);
-            Serial.print(",");
-            Serial.print(adcbuffer_0[i + 2]);
-            Serial.print(",");
-            Serial.print(adcbuffer_0[i + 3]);
-            if (i != BUF_SIZE - 4)
-              Serial.print(",");
-          }
-          Serial.print("], \"ResultADC1\":[");
-          for (int i = 0; i < BUF_SIZE; i = i + 4) {
-            Serial.print(adcbuffer_1[i]);
-            Serial.print(",");
-            Serial.print(adcbuffer_1[i + 1]);
-            Serial.print(",");
-            Serial.print(adcbuffer_1[i + 2]);
-            Serial.print(",");
-            Serial.print(adcbuffer_1[i + 3]);
-            if (i != BUF_SIZE - 4)
-              Serial.print(",");
-          }
-          Serial.print("]}\n");
-
-          digitalWrite(ledPin, HIGH);   // set the LED on
-          delay(50);                  // wait for a second
-          digitalWrite(ledPin, LOW);    // set the LED off
-          break;
         }
-      case 2: {
-          //Start a capture/conversion
+        Serial.print("]}\n");
 
-          //First, load the channels to sample from the command
-          for (int i(0); i < 4; ++i) {
-            ChannelsCfg_0[i] = 0x40 | json_in_root["ADC0Channels"][i].as<uint16_t>();
-            ChannelsCfg_1[i] = 0x40 | json_in_root["ADC1Channels"][i].as<int>();
-          }
+        digitalWrite(ledPin, HIGH);   // set the LED on
+        delay(50);                  // wait for a second
+        digitalWrite(ledPin, LOW);    // set the LED off
+        break;
+      }
+    case 2: {
+        //Start a capture/conversion
 
-          pwm_pin = json_in_root["PWM_pin"];
-          pwm_pulse_width = json_in_root["PWMwidth"];
-          pwm_counter = 0;
-          if (pwm_pin > -1)
-             digitalWrite(pwm_pin, 0);
-          //Don't allow interrupts while we enable all the dma systems
-          noInterrupts();
-          dma0->enable();
-          dma2->enable();
-          if (pwm_pin > -1)
-            pwm_timer.begin(pwm_isr, 12);  // blinkLED to run every 0.15 seconds
-          interrupts();
-
-          jsonBuffer.clear(); //Save memory by clearing the jBuffer for reuse, we can't use json_in_root or anything from it after this though!
-          JsonObject& json_out_root = jsonBuffer.createObject();
-          json_out_root["Status"] = "Success";
-
-          json_out_root.printTo(Serial);
-          break;
+        //First, load the channels to sample from the command
+        for (int i(0); i < 4; ++i) {
+          ChannelsCfg_0[i] = 0x40 | json_in_root["ADC0Channels"][i].as<uint16_t>();
+          ChannelsCfg_1[i] = 0x40 | json_in_root["ADC1Channels"][i].as<int>();
         }
-      default: {
-          jsonBuffer.clear(); //Save memory by clearing the jBuffer for reuse, we can't use json_in_root or anything from it after this though!
-          JsonObject& json_out_root = jsonBuffer.createObject();
-          json_out_root["Status"] = "Fail";
-          json_out_root["Error"] = "Unrecognised command";
-          json_out_root.printTo(Serial);
-          Serial.print('\n');
-          break;
+
+        pwm_pin = json_in_root["PWM_pin"];
+        pwm_pulse_width = json_in_root["PWMwidth"];
+        pwm_counter = 0;
+        if (pwm_pin > -1) {
+          pinMode(pwm_pin, OUTPUT);
+          digitalWrite(pwm_pin, 0);
         }
-    }
+        //Don't allow interrupts while we enable all the dma systems
+        noInterrupts();
+        dma0->enable();
+        dma2->enable();
+        if (pwm_pin > -1)
+          pwm_timer.begin(pwm_isr, 12);  // blinkLED to run every 0.15 seconds
+        interrupts();
+
+        jsonBuffer.clear(); //Save memory by clearing the jBuffer for reuse, we can't use json_in_root or anything from it after this though!
+        JsonObject& json_out_root = jsonBuffer.createObject();
+        json_out_root["Status"] = "Success";
+
+        json_out_root.printTo(Serial);
+        break;
+      }
+    default: {
+        Serial.print("{\"Status\":\"Fail\", \"Error\":\"Unrecognised command\"}\n");
+        break;
+      }
   }
 
   digitalWrite(ledPin, !digitalRead(ledPin));   //Toggle
