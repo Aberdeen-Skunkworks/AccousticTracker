57f7a4aa7632404922368a90bedc11e582c79712
NiallMacD <niall.macdonald5@gmail.com>
2018-05-27 00:26:05 +0100

    Much nicer plotting and updating - fully commented

--- PythonController/Main.py
+++ PythonController/Main.py
@@ -53,7 +53,7 @@ if choose == ("1"):
 
     # Set up plotting axis
     ax = plt.gca()
-    li = [plt.plot([1,1], 'x-')[0] for i in range(3)]
+    li = [plt.plot([1,1], 'x-')[0] for i in range(4)]
     
     while True:
         #Trigger the average funvtion to take readings with the following command
@@ -125,12 +125,14 @@ if choose == ("1"):
 elif choose == ("2"):
     
     # Set up plotting axis
-
     fig = plt.figure()
     ax1 = fig.add_subplot(2,1,1)
     ax2 = fig.add_subplot(2,1,2)
     
-    li = [ax1.plot([1,1], 'x-')[0] for i in range(8)]
+    # Create the lines and points that can be updated every loop. This means the figure does not need cleared every time its updated
+    li = [ax1.plot([1,1], 'x-')[0] for i in range(4)]
+    li_2 = [ax2.plot([1,1], 'x-')[0] for i in range(3)]
+    points = [ax2.plot([1,1], 'o', markersize=12)[0] for i in range(3)]
     
     # List of commands that cycle through what transducer is pinging and which is recieving
     command_list = [
@@ -142,21 +144,24 @@ elif choose == ("2"):
     
     {"CMD":2, "ADC0Channels":[23,23,23,23], "ADC1Channels":[38,38,38,38], "PWM_pin":22, "PWMwidth":6},
     {"CMD":2, "ADC0Channels":[23,23,23,23], "ADC1Channels":[38,38,38,38], "PWM_pin":20, "PWMwidth":6}]
+    
+    # List of what ADC the target wave will be on
     target_wave_adc0_or_adc1_list = [0,1,1,0,1,0]
     
 
     while True: 
     
-        #Keeping track of distances
+        #Keeping track of all 6 distances: 1-2, 2-1, 1-3, 3-1, 2-3, 3-2
         all_distances = []
         
+        # Looping through the 6 distances to calculate
         for mesurment in range(6):
             
             # Set up changing variables
             command = command_list[mesurment]
             target_wave_adc0_or_adc1 = target_wave_adc0_or_adc1_list[mesurment]
             
-            #Trigger the average funvtion to take readings with the following command
+            #Trigger the average funvtion to take readings with the command from the list
             background_voltage_0, background_voltage_1 = find_background_voltages(command, adc_resolution)
             voltages_adc_0_not_scaled, voltages_adc_1_not_scaled = average_waves(5, adc_resolution, command)
             voltages_adc_0, voltages_adc_1 = scale_around_zero(background_voltage_0, background_voltage_1, voltages_adc_0_not_scaled, voltages_adc_1_not_scaled, adc_resolution)
@@ -210,7 +215,7 @@ elif choose == ("2"):
             li[3].set_label(distance_label)
             
             
-            # Commands and labels for plotting the data continioustly
+            # Commands and labels for plotting the data continioustly for axis 1
             ax1.legend()
             ax1.set_ylim([-2,2]) 
             ax1.set_ylabel('Voltage (V)')
@@ -220,7 +225,7 @@ elif choose == ("2"):
             ax1.autoscale_view(True,True,True)
             plt.pause(0.01)
                 
-            
+        # Average the repeated distances and print to output
         distance_1 = np.average([all_distances[0], all_distances[1]])
         distance_2 = np.average([all_distances[2], all_distances[3]])
         distance_3 = np.average([all_distances[4], all_distances[5]])
@@ -229,11 +234,12 @@ elif choose == ("2"):
         print("D2 1-3 = Average = ", "%.2f" % distance_2)
         print("D3 2-3 = Average = ", "%.2f" % distance_3)
 
-
+        # Performing trig funcitons on the distances to calcuate their relitive positions: H = height of triangle from transducer 3 to the x axis. L = distance between y axis and transducer 3
         angle = math.acos((distance_1**2 + distance_2**2 - distance_3**2)/(2*distance_2*distance_1))
         L = math.cos(angle)*distance_2
         H = math.sin(angle)*distance_2
         
+        # Creating the dynamic labels that update what the distances are and are displayed in the legend
         distance_str_1 = str("%.2f" % distance_1)
         distance_label_1 = "Distance between 1 and 2: " + distance_str_1 + " cm"
         distance_str_2 = str("%.2f" % distance_2)
@@ -241,8 +247,7 @@ elif choose == ("2"):
         distance_str_3 = str("%.2f" % distance_3)
         distance_label_3 = "Distance between 2 and 3: " + distance_str_3 + " cm"
         
-        ax2.clear()
-        li_2 = [ax2.plot([1,1], 'x-')[0] for i in range(3)]
+        # Updating the line and point data for all three lines and points
         li_2[0].set_ydata([0, 0])
         li_2[0].set_xdata([0, distance_1])
         li_2[0].set_label(distance_label_1)
@@ -255,16 +260,25 @@ elif choose == ("2"):
         li_2[2].set_xdata([distance_1, L])
         li_2[2].set_label(distance_label_3)
         
-        x_values = [0, distance_1 , L]
-        y_values = [0, 0          , H]
+        points[0].set_ydata([0])
+        points[0].set_xdata([0])
+        points[0].set_label("Transducer 1")
         
+        points[1].set_ydata([0])
+        points[1].set_xdata([distance_1])
+        points[1].set_label("Transducer 2")
+        
+        points[2].set_ydata([H])
+        points[2].set_xdata([L])
+        points[2].set_label("Transducer 3")
+        
+        # Commands and labels for plotting the data continioustly for axis 2
+        ax2.relim()       
         ax2.autoscale_view(True,True,True)
+        ax2.set_ylim([-0.5,10]) 
+        ax2.set_xlim([-0.5,10]) 
         plt.gcf().canvas.draw()
         plt.pause(0.01)
-        
-        ax2.plot(x_values, y_values ,'ro', markersize=20)
-        ax2.set_ylim(-0.5,10)
-        ax2.set_xlim(-0.5,10)
         ax2.set_title('Locations of transducers')
         ax2.set_xlabel('x axis m')
         ax2.set_ylabel('y axis m')
